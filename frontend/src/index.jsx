f9257b925c75544cbc62b9e7ad9bd60b0369b5c1
Lakshya Shastri <lakshyashastri11@gmail.com>
2023-01-22 05:27:37 +0530

    login page

---
+++ frontend/src/index.jsx
@@ -0,0 +1,135 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+import logo from "./assets/4reddit_logo.png";
+import logoGIF from "./assets/obamasphere.gif";
+
+import CssBaseline from "@mui/material/CssBaseline";
+import Grid from "@mui/material/Grid";
+import Typography from "@mui/material/Typography";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+import Box from "@mui/material/Box";
+
+import LoginIcon from "@mui/icons-material/Login";
+
+// import "./index.css";
+
+// temporary since importing not working: shift to that later since I can edit css file better there
+let styles = {
+    logo: {
+        height: "256px",
+        width: "256px",
+        marginTop: "20px",
+        borderRadius: "50%"
+    },
+    flexbox: {
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center"
+    },
+    inputField: {m: 0.5, width: 400, input: {color: "rgb(51, 118, 204)"}}
+};
+
+class Logo extends React.Component {
+    state = {
+        hovering: false
+    };
+
+    render() {
+        return (
+            <React.Fragment>
+                <div style={styles.flexbox}>
+                    <img
+                        src={this.state.hovering ? logoGIF : logo}
+                        onMouseOver={() => this.setState({hovering: true})}
+                        onMouseOut={() => this.setState({hovering: false})}
+                        style={styles.logo}
+                    />
+                </div>
+            </React.Fragment>
+        );
+    }
+}
+
+function HeadingText(props) {
+    return (
+        <Typography component="h1" variant="h3" sx={{m: 1}}>
+            Sign in
+        </Typography>
+    );
+}
+
+class LoginForm extends React.Component {
+    state = {
+        login: true // false if registeration required
+    };
+
+    render() {
+        return (
+            <div
+                style={{
+                    ...styles.flexbox,
+                    flexDirection: "column",
+                    justifyContent: "space-evenly"
+                }}
+            >
+                <TextField
+                    label="Username"
+                    variant="outlined"
+                    sx={styles.inputField}
+                />
+                <TextField
+                    label="Password"
+                    variant="outlined"
+                    type="password"
+                    sx={styles.inputField}
+                />
+                <Button
+                    variant="contained"
+                    size="large"
+                    endIcon={<LoginIcon />}
+                    sx={{mt: 1}}
+                    disabled={1 ? false : true}
+                >
+                    Sign in
+                </Button>
+            </div>
+        );
+    }
+}
+
+function BottomText(props) {
+    return (
+        <Box sx={{textAlign: "center"}}>
+            <Typography component="h6" variant="h6">
+                Don't have an account?
+            </Typography>
+            <Button variant="text" size="small">
+                Register
+            </Button>
+        </Box>
+    );
+}
+class LoginPage extends React.Component {
+    render() {
+        return (
+            <React.Fragment>
+                <CssBaseline />
+                <Logo />
+                <Box component="form" sx={{m: 1, mb: 3, textAlign: "center"}}>
+                    <HeadingText />
+                    <LoginForm />
+                </Box>
+                <BottomText />
+            </React.Fragment>
+        );
+    }
+}
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<LoginPage />);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// reportWebVitals();
