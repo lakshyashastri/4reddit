1ea5a7dbb4c3cdcc9d3cbed9129524aaf24f7c4b
Lakshya Shastri <lakshyashastri11@gmail.com>
2023-02-13 05:11:37 +0530

    post object shows no posts when no posts

--- TODO.md
+++ TODO.md
@@ -3,3 +3,10 @@
 - Calculate basename depending on nodejs environment OS
 
 - loading profile page loads "user not found" for a second
+
+
+today
+- use cases -> 1 -> b (easy marks)
+- use cases -> 2 (easy marks: just alter response from backend)
+- use cases -> 1 -> c -> ii -> 1 -> a
+- use cases -> 1 -> d -> 2 -> g (EASY)
--- frontend/src/components/Posts.jsx
+++ frontend/src/components/Posts.jsx
@@ -46,110 +46,131 @@ export default function Posts(props) {
     return (
         <div>
             {postData ? (
-                postData.map(post => {
-                    return (
-                        <Accordion
-                            expanded={expanded === post.id}
-                            onChange={handleChange(post.id)}
-                            sx={{
-                                maxWidth: props.saved ? 1500 : 1000,
-                                minWidth: props.saved ? 1200 : 800
-                            }}
-                            key={post.id}
-                        >
-                            <AccordionSummary expandIcon={<ExpandMoreIcon />}>
-                                <Typography sx={{width: "27%", flexShrink: 0}}>
-                                    <VoteButton postData={post} upvote />
-                                    <VoteButton postData={post} downvote />
-                                </Typography>
-                                <Typography
-                                    sx={{
-                                        color: "text.secondary",
-                                        width: "63%"
-                                    }}
+                postData.length == 0 ? (
+                    <Grid
+                        spacing={0}
+                        direction="column"
+                        alignItems="center"
+                        justifyContent="center"
+                        sx={{minHeight: "10vh"}}
+                        container
+                    >
+                        <Grid item>
+                            <Typography variant="h3" align="center">
+                                No posts
+                            </Typography>
+                        </Grid>
+                    </Grid>
+                ) : (
+                    postData.map(post => {
+                        return (
+                            <Accordion
+                                expanded={expanded === post.id}
+                                onChange={handleChange(post.id)}
+                                sx={{
+                                    maxWidth: props.saved ? 1500 : 1000,
+                                    minWidth: props.saved ? 1200 : 800
+                                }}
+                                key={post.id}
+                            >
+                                <AccordionSummary
+                                    expandIcon={<ExpandMoreIcon />}
                                 >
-                                    {post.title}
-                                </Typography>
-                                <Typography
-                                    sx={{
-                                        color: "text.secondary",
-                                        flexShrink: 0
-                                    }}
-                                >
-                                    {props.saved
-                                        ? `Posted by ${post.postedBy} in r/${post.postedIn}`
-                                        : props.boardit
-                                        ? `Posted by ${post.postedBy}`
-                                        : props.user
-                                        ? `Posted in r/${post.postedIn}`
-                                        : null}
-                                    {" on "}
-                                    {post.createdAt
-                                        .split("T")[0]
-                                        .split("-")
-                                        .reverse()
-                                        .join("-")}
-                                </Typography>
-                            </AccordionSummary>
-                            <AccordionDetails>
-                                <Typography>{post.text}</Typography>
-                                <Grid
-                                    sx={{
-                                        display: "flex",
-                                        justifyContent: "space-evenly"
-                                    }}
-                                    spacing={1}
-                                    container
-                                >
-                                    <Grid item>
-                                        <Button
-                                            variant="contained"
-                                            color="info"
-                                            style={{marginTop: 10}}
-                                            onClick={() =>
-                                                setShowAddCommentModal(true)
+                                    <Typography
+                                        sx={{width: "27%", flexShrink: 0}}
+                                    >
+                                        <VoteButton postData={post} upvote />
+                                        <VoteButton postData={post} downvote />
+                                    </Typography>
+                                    <Typography
+                                        sx={{
+                                            color: "text.secondary",
+                                            width: "63%"
+                                        }}
+                                    >
+                                        {post.title}
+                                    </Typography>
+                                    <Typography
+                                        sx={{
+                                            color: "text.secondary",
+                                            flexShrink: 0
+                                        }}
+                                    >
+                                        {props.saved
+                                            ? `Posted by ${post.postedBy} in r/${post.postedIn}`
+                                            : props.boardit
+                                            ? `Posted by ${post.postedBy}`
+                                            : props.user
+                                            ? `Posted in r/${post.postedIn}`
+                                            : null}
+                                        {" on "}
+                                        {post.createdAt
+                                            .split("T")[0]
+                                            .split("-")
+                                            .reverse()
+                                            .join("-")}
+                                    </Typography>
+                                </AccordionSummary>
+                                <AccordionDetails>
+                                    <Typography>{post.text}</Typography>
+                                    <Grid
+                                        sx={{
+                                            display: "flex",
+                                            justifyContent: "space-evenly"
+                                        }}
+                                        spacing={1}
+                                        container
+                                    >
+                                        <Grid item>
+                                            <Button
+                                                variant="contained"
+                                                color="info"
+                                                style={{marginTop: 10}}
+                                                onClick={() =>
+                                                    setShowAddCommentModal(true)
+                                                }
+                                            >
+                                                Add comment
+                                            </Button>
+                                        </Grid>
+                                        <Modal
+                                            sx={{marginTop: 3}}
+                                            open={showAddCommentModal}
+                                            onClose={() =>
+                                                setShowAddCommentModal(false)
                                             }
                                         >
-                                            Add comment
-                                        </Button>
+                                            <Fade in={showAddCommentModal}>
+                                                <Box sx={modalStyling}>
+                                                    <NewCommentModal
+                                                        modalFunc={
+                                                            setShowAddCommentModal
+                                                        }
+                                                        postID={post.id}
+                                                    />
+                                                </Box>
+                                            </Fade>
+                                        </Modal>
+                                        <Grid item>
+                                            <SavePost post={post} />
+                                        </Grid>
+                                        <Grid item>
+                                            <Button
+                                                variant="contained"
+                                                color="info"
+                                                style={{marginTop: 10}}
+                                                // onClick={}
+                                            >
+                                                View comments (
+                                                {post.comments.length})
+                                            </Button>
+                                        </Grid>
                                     </Grid>
-                                    <Modal
-                                        sx={{marginTop: 3}}
-                                        open={showAddCommentModal}
-                                        onClose={() =>
-                                            setShowAddCommentModal(false)
-                                        }
-                                    >
-                                        <Fade in={showAddCommentModal}>
-                                            <Box sx={modalStyling}>
-                                                <NewCommentModal
-                                                    modalFunc={
-                                                        setShowAddCommentModal
-                                                    }
-                                                    postID={post.id}
-                                                />
-                                            </Box>
-                                        </Fade>
-                                    </Modal>
-                                    <Grid item>
-                                        <SavePost post={post} />
-                                    </Grid>
-                                    <Grid item>
-                                        <Button
-                                            variant="contained"
-                                            color="info"
-                                            style={{marginTop: 10}}
-                                            // onClick={}
-                                        >
-                                            View comments (
-                                            {post.comments.length})
-                                        </Button>
-                                    </Grid>
-                                </Grid>
-                            </AccordionDetails>
-                        </Accordion>
-                    );
-                })
+                                </AccordionDetails>
+                            </Accordion>
+                        );
+                    })
+                )
             ) : (
                 <Loading />
             )}
